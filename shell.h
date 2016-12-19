77ffc3f8e8f8c9ac3217cbeae8e6471596c7a78f
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-12-19 23:11:16 +0000

    updated header file.

--- shell.h
+++ shell.h
@@ -1,3 +1,5 @@
 char *_getenv(const char *name, char **environ);
 int _strcmp(const char *s1, char *s2);
 char *printenv(char **environ);
+int _setenv(const char *name, const char *value);
+int _unsetenv(const char *name);
